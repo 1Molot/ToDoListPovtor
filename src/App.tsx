c07741c1504f121f326d50ba582f35135137f93d
1Molot <sinkevich1990@gmail.com>
2023-03-27 21:40:41 +0300

    fix: PN-1Yrock

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todolist, {TaskType} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const todoListTitle_1: string = "What to learn"
+    const todoListTitle_2: string = "What to buy"
+
+    const tasks_1: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+    ]
+    const tasks_2: Array<TaskType> = [
+        {id: 4, title: "BREAD", isDone: true},
+        {id: 5, title: "WATER", isDone: false},
+        {id: 6, title: "SALT", isDone: false},
+    ]
+    return (
+        <div className="App">
+            <Todolist
+                title={todoListTitle_1}
+                tasks={tasks_1}
+            />
+            <Todolist
+                tasks={tasks_2}
+                title={todoListTitle_2}/>
+
+        </div>
+    );
 }
 
 export default App;
